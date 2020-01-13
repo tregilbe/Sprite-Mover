c690803fcc7f87efc9d460f65a7208a23817c488
Trenton <tregilbe@uat.edu>
2020-01-13 13:19:29 -0700

    Added sprite and script

---
+++ Assets/Scripts/SpriteChanger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteChanger : MonoBehaviour
+{
+    // Declare variables at the top
+    private SpriteRenderer spriteRenderer;
+    public Color color = Color.green;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.color = Color.green;
+        }
+        else
+        {
+            Debug.LogError(message:"No Sprite Renderer Attached");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.color = color;
+        }
+        else
+        {
+            Debug.LogError(message: "No Sprite Renderer Attached");
+        }
+
+    }
+}
