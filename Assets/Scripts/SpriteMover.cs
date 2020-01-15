2ea5b9ad1a138d1d0cdb4f516e067d90358162e7
Trenton <tregilbe@uat.edu>
2020-01-15 12:18:23 -0700

    Added movement script for up

---
+++ Assets/Scripts/SpriteMover.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteMover : MonoBehaviour
+{
+    private Transform tf;
+
+    public float speed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tf = gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            // Only works if shift is held down.
+        }
+        else
+        {
+            // Only works if shift is not held down.
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                tf.position = tf.position + Vector3.up * Time.deltaTime * speed;
+            }
+
+        }
+    }
+}
